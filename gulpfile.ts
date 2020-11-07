71d525a03bf6b4274e0ae07405ad6dd2b73392c6
brisberg <brandon.risberg@gmail.com>
2020-11-06 22:29:10 -0800

    Fixed default build task

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,6 @@
 import {exec} from 'child_process';
 import * as fs from 'fs';
-import {dest, parallel, series, src} from 'gulp';
+import {dest, series, src} from 'gulp';
 import clean from 'gulp-clean';
 import concat from 'gulp-concat';
 import filter from 'gulp-filter';
@@ -29,13 +29,16 @@ function isJavaScript(file) {
   return file.extname === '.js';
 }
 
+function genHeaderFile(): NodeJS.ReadWriteStream {
+  return concatHeaderFile(config.header);
+}
+
 /** Concatinates all js and html files for the given glob into single html. */
 function concatHeaderFile(
     glob: string|string[], outdir = DEFAULT_OUT_DIR): NodeJS.ReadWriteStream {
-  const globs = ['header/builder.html'].concat(glob);
-  console.log(globs);
-  return src(glob)
-      .pipe(filter('*', {debug: true}))
+  // Append twine-builder standard header
+  return src(['header/builder.html'].concat(glob))
+      .pipe(filter('**/*.{js,html}'))
       // Wrap naked js files in <script> tags when appended to the header
       .pipe(gulpif(isJavaScript, header('<script>\n')))
       .pipe(gulpif(isJavaScript, footer('</script>')))
@@ -43,20 +46,14 @@ function concatHeaderFile(
       .pipe(dest(outdir));
 }
 
-export function build(): NodeJS.ReadWriteStream {
-  return concatHeaderFile(config.header || []);
-}
-build.displayName = 'Build';
-build.description = 'Builds the Game.';
-
 function tweego(done: (error?: any) => void) {
   const deps: string[] = [].concat(config.deps)
   const cmd = `tweego --log-files -l \
   --format=${config.format} \
   --head=output/head-content.html \
-  -o dist/game.html \
+  -o dist/${config.id}.html \
   ${deps.join(' ')}
-  `
+  `;
   exec(cmd, function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -64,12 +61,12 @@ function tweego(done: (error?: any) => void) {
   });
 }
 
-// function defaultTask(): NodeJS.ReadWriteStream {
-//   return src(DEFAULT_CONFIG_PATH).pipe(dest('./output/'));
-// }
-
 function clear(): NodeJS.ReadWriteStream {
   return src(DEFAULT_OUT_DIR, {read: false, allowEmpty: true}).pipe(clean());
 }
 
-export default series(clear, build, tweego);
+const build = series(clear, genHeaderFile, tweego);
+build.displayName = 'build';
+build.description = 'Compiles the game using Tweego';
+
+export default build;
