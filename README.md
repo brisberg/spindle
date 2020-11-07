19ef4437d1bd998269e280d647366d65f791a72b
brisberg <brandon.risberg@gmail.com>
2020-11-06 18:39:44 -0800

    Extend gulp file to concatinate header.html file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # twine-builder
 WIP Project porting my opinionated twine builder to a modular system.
 
+Reimplementing as a Gulp Task
+
 Possible ideas for name of this project:
 
   - `entwine`: The verb of wrapping something in twine, meshing things together
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,27 @@
-export default function defaultTask(cb: Function): void {
-  // place code for your default task here
-  cb();
+import * as fs from 'fs';
+import {dest, parallel, src} from 'gulp';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import * as yaml from 'js-yaml';
+
+let data: any = {head: []};
+try {
+  let fileContents = fs.readFileSync('./example.yml', 'utf8');
+  data = yaml.safeLoad(fileContents);
+
+  console.log(data);
+} catch (e) {
+  console.log(e);
 }
 
-// exports.default = defaultTask
+function concatHeaderFile(): NodeJS.ReadWriteStream {
+  return src(data.head, {allowEmpty: true})
+      .pipe(concat('head-content.html'))
+      .pipe(dest('./output'));
+}
+
+function defaultTask(): NodeJS.ReadWriteStream {
+  return src('example.yml').pipe(dest('./output/'));
+}
+
+export default parallel(defaultTask, concatHeaderFile);
